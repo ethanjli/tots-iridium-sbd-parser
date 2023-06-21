4854f4999ca5e2542b2fbcf4508d5ed32f89d9f0
Ethan Li <lietk12@gmail.com>
2023-06-21 13:07:46 -0800

    Handle all MO message types

--- sbd/sbd.py
+++ sbd/sbd.py
@@ -1,31 +1,25 @@
 # -*- coding: utf-8 -*-
 """Parser for Iridium SBD messages"""
 
+import binascii
 import collections
 
-class Message():
-    """Represents an Iridium SBD message as a raw byte sequence."""
-    def __init__(self, content):
-        self.content = content
-        self.offset = 0
+_sbd_mo_message_types = {
+    0x00: 'unencrypted-position',
+    0x01: 'encrypted-position',
+    0x02: 'encrypted-tlv-data',
+    0x1f: 'pad-tlv',
+    0x23: 'config-updated',
+    0x2e: 'user-data',
+    0x35: 'unencrypted-tlv-data',
+    0x37: 'unencrypted-chained-position',
+    0x38: 'encrypted-chained-position',
+    0x39: 'unencrypted-engineering',
+    0x3a: 'encrypted-engineering',
+    0x4a: 'nack',
+}
 
-    def __str__(self):
-        return str(self.content)
-
-    def __len__(self):
-        return len(self.content)
-
-    def __getitem__(self, item):
-        """
-            Avoids to convert to string when requesting only 1 item,
-              like msg[0]
-        """
-        if (isinstance(item, int) and (item != -1)):
-            item = slice(item, item+1)
-        return self.content[self.offset:][item]
-
-
-_message_types = {
+_solar_edge_message_types = {
     0x00: 'radio-silence-in',
     0x01: 'radio-silence-out',
     0x03: 'start-motion',
@@ -36,31 +30,11 @@ _message_types = {
     0x1b: 'position',
 }
 
-
 class IridiumSBD():
     """Parses an Iridium SBD messge.
 
-    Iridium transmit SBD messages through DirectIP using its own binary format.
-    This class gives a comprehensible object.
-
     Attributes:
         header: A dictionary with the section header of an ISBD message.
-        payload: A dictionary with the section payload of an ISBD message.
-        confirmation: A dictionary with the section confirmation of an ISBD
-            message.
-        mtype: Message type, MO or MT.
-
-    One can run this as:
-
-    >>> isbd = IridiumSBD(msg)
-    >>> isbd.header # {'IEI': ..., 'length': ...}
-    >>> isbd.payload
-    >>> isbd.confirmation
-
-    >>> isbd.mtype # 'MO' | 'MT'
-
-    >>> isbd.decode() # Parse the binary message
-    >>> isbd.encode() # Encode proprieties into a binary message
     """
     def __init__(self, msg=None):
         """Initialize an IridiumSBD object.
@@ -69,14 +43,13 @@ class IridiumSBD():
             msg (byte): A binary ISBD message (optional). If given, runs
                 load(msg).
         """
-        self.mtype = None
         if msg is not None:
             self.load(msg)
 
     def __str__(self):
         return str(self.attributes)
 
-    def load(self, msg):
+    def load(self, raw):
         """Parse an Iridium SBD binary message.
 
         Args:
@@ -100,19 +73,14 @@ class IridiumSBD():
 
         What is the syntax for the MO Receipt Confirmation???
         """
-        self.msg = Message(msg)
-
         self.attributes = {'header': collections.OrderedDict()}
-        raw = bytes(self.msg[:])
 
-        if self.msg[0] != b'\x01':
-            print(f'Message type: 0x{raw[0]:x}')
-            assert False, 'Unknown message type'
+        message_type =  _sbd_mo_message_types[raw[0]]
+        self.attributes['message-type'] = message_type
 
-        print('Parsing MO encrypted position message...')
-        print(f'Iridium Edge Solar Message header: 0b{raw[1]:b}')
+        self.header = raw[1]
 
-        message_type = _message_types[raw[1] & 0b00011111]
+        message_type = _solar_edge_message_types[raw[1] & 0b00011111]
         self.attributes['header']['message-type'] = message_type
         if message_type == 'radio-silence-in':
             self.attributes['header']['message-count'] = (raw[1] & 0b11000000) >> 6
@@ -148,8 +116,11 @@ class IridiumSBD():
 def dump(file):
     """Show isbd message header as text
     """
-    msg = IridiumSBD(file.read())
+    raw = file.read()
+    print(f'Parsing MO message {binascii.hexlify(raw, " ")}...')
+    msg = IridiumSBD(raw)
 
-    print('Header section:')
+    print(f'message-type: {msg.attributes["message-type"]}')
+    print(f'Header 0b{msg.header:b}:')
     for value in msg.attributes['header']:
         print(f'  - {value}: {msg.attributes["header"][value]}')
